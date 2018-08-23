98323f6af505474e451237039923fb5dd16dea5a
rmanicome <rmanicome@gmail.com>
2018-08-23 17:23:03 +0200

    [Feature] Working update computers

--- src/app/computers/routed/computers-update/computers-update.component.ts
+++ src/app/computers/routed/computers-update/computers-update.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ComputersService } from '../../shared/computers.service';
 import { CompaniesService } from '../../../companies/shared/companies.service';
 import { Computer } from '../../../shared/models/computer.model';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { Company } from '../../../shared/models/company.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-computers-update',
@@ -12,9 +12,13 @@ import { Company } from '../../../shared/models/company.model';
   styleUrls: ['./computers-update.component.scss']
 })
 export class ComputersUpdateComponent implements OnInit {
-  computer: Computer;
+  @Input() computer: Computer;
   companies: Company[];
   name = new FormControl('', [Validators.required]);
+  introducedDate: Date;
+  discontinuedDate: Date;
+  companyId: number;
+  today = new Date();
 
   updateForm = this._fb.group({
     id: [''],
@@ -25,35 +29,64 @@ export class ComputersUpdateComponent implements OnInit {
   });
 
   constructor(
-    private _route: ActivatedRoute,
     private _computersService: ComputersService,
     private _companiesService: CompaniesService,
-    private _fb: FormBuilder
+    private _fb: FormBuilder,
+    public snackBar: MatSnackBar
   ) { }
 
   ngOnInit() {
-    const id = this._route.snapshot.paramMap.get('id');
+    this.today.setDate(this.today.getDate() - 1);
     this._companiesService.getAllCompanies().subscribe(companies => this.companies = companies);
-    this._computersService.getById(id).subscribe(computer => {
-      this.computer = computer;
-      console.log(computer);
-      this.updateForm.patchValue({
-        id: this.computer.id,
-        name: this.computer.name,
-        introduced: this.computer.introducedDate,
-        discontinued: this.computer.discontinuedDate,
-        company: this.computer.company
-      });
+    if (this.computer.introducedDate != null) {
+      this.introducedDate = new Date(1970, 0, 1);
+      this.introducedDate.setMilliseconds(Date.parse(this.computer.introducedDate));
+    }
+    if (this.computer.discontinuedDate != null) {
+      this.discontinuedDate = new Date(1970, 0, 1);
+      this.discontinuedDate.setMilliseconds(Date.parse(this.computer.discontinuedDate));
+    }
+    this.updateForm.patchValue({
+      id: this.computer.id,
+      name: this.computer.name,
+      introduced: this.introducedDate,
+      discontinued: this.discontinuedDate,
+      company: this.computer.company
     });
   }
 
-  sendForm() {
-    this.computer.name = this.updateForm.get('name').value;
-    this.computer.introducedDate = this.updateForm.get('introduced').value;
-    this.computer.discontinuedDate = this.updateForm.get('discontinued').value;
-    this.computer.company = this.updateForm.get('company').value;
+  update() {
+    if (!this.name.hasError('required')) {
+      if (this.companyId != null) {
+        this._companiesService.getById(this.companyId.toString()).subscribe(company => this.computer.company = company);
+      } else {
+        this.computer.company = null;
+      }
 
-    this._computersService.update(this.computer).subscribe();
+      if (this.introducedDate != null) {
+        this.introducedDate.setDate(this.introducedDate.getDate() + 1);
+        this.computer.introducedDate = this.introducedDate.toJSON().substring(0, 10);
+      }
+
+      if (this.discontinuedDate != null) {
+        this.discontinuedDate.setDate(this.discontinuedDate.getDate() + 1);
+        this.computer.discontinuedDate = this.discontinuedDate.toJSON().substring(0, 10);
+      }
+
+      this._computersService.update(this.computer).subscribe(
+        () => {
+          this.snackBar.open('The computer has been updated', '', {
+            duration: 1000,
+          });
+        },
+        error => {
+          console.log(error);
+          this.snackBar.open('An error occured', '', {
+            duration: 1000,
+          });
+        }
+      );
+    }
   }
 
   getNameErrorMessage() {
