1958b272bb6ca4711b2e743ee0558eed5366e7ba
rmanicome <rmanicome@gmail.com>
2018-09-13 11:12:04 +0200

    Change of the front to match material design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { TranslateService } from '@ngx-translate/core';
+import { ProgressBarComponent } from '../shared/progress-bar/progress-bar.component';
 
 @Component({
   // selector: 'app-login',
@@ -12,32 +15,36 @@ export class LoginComponent {
   username: string;
   password: string;
 
-  constructor(public authService: AuthService, public router: Router) {
-    this.setMessage();
-
-  }
-
-  setMessage() {
-    this.message =  (this.authService.isLoggedIn ? 'welcome, ' + this.authService.name : ' logged out');
-  }
+  constructor(
+    public authService: AuthService,
+    public router: Router,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    private _translate: TranslateService
+  ) { }
 
   login() {
-    this.message = 'Trying to log in ...';
+    const dialogRef = this.dialog.open(ProgressBarComponent);
+    dialogRef.disableClose = true;
 
     this.authService.login(this.username, this.password).subscribe(() => {
+      dialogRef.close();
       if (this.authService.isLoggedIn) {
-        this.setMessage();
         const redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/computers';
+        this.snackBar.open(this._translate.instant('welcome') + ' ' + this.username, '', {
+          duration: 1000,
+        });
         // Redirect the user
         this.router.navigate([redirect]);
       } else {
-        this.message = 'error in username or password.';
+        this.snackBar.open(this._translate.instant('error.id'), '', {
+          duration: 1000,
+        });
       }
     });
   }
 
   logout() {
     this.authService.logout();
-    this.setMessage();
   }
 }
